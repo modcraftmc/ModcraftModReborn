01002e66ed6615c5ba6d0f90af489f879690a3c8
manugame <manugameur.pro@gmail.com>
2023-10-31 21:41:03 +0100

    make this null-safe when switching servers

--- src/main/java/fr/modcraftmc/modcraftmod/mixin/MinecraftMixin.java
+++ src/main/java/fr/modcraftmc/modcraftmod/mixin/MinecraftMixin.java
@@ -2,9 +2,14 @@ package fr.modcraftmc.modcraftmod.mixin;
 
 import fr.modcraftmc.modcraftmod.ModcraftModReborn;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.Connection;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.jar.Attributes;
@@ -12,6 +17,10 @@ import java.util.jar.Manifest;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
+
+    @Shadow @Nullable public LocalPlayer player;
+
+    @Shadow @Nullable public Connection pendingConnection;
 
     /**
      * @author manugame_
@@ -21,5 +30,23 @@ public class MinecraftMixin {
     public String createTitle() {
         Attributes manifest = ModcraftModReborn.getManifest().getMainAttributes();
         return String.format("ModcraftMC: Reborn (ver: %s-%s)", manifest.getValue("Release-Type"),  manifest.getValue("Build-Time"));
+    }
+
+    /**
+     * @author manugame_
+     * @reason make this null-safe when switching servers
+     */
+    @Overwrite
+    public ClientPacketListener getConnection() {
+        if (this.player == null) {
+            if (this.pendingConnection == null) {
+                return null;
+            } else {
+                this.pendingConnection.getPacketListener();
+            }
+        } else {
+            return this.player.connection;
+        }
+        return null;
     }
 }
