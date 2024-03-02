d8f8b5765dee3b94f94811e3645df6f37d60f1f9
manugame <manugameur.pro@gmail.com>
2024-03-02 17:08:36 +0100

    re-add fast world loading

--- src/main/java/fr/modcraftmc/modcraftmod/mixin/MinecraftMixin.java
+++ src/main/java/fr/modcraftmc/modcraftmod/mixin/MinecraftMixin.java
@@ -1,13 +1,20 @@
 package fr.modcraftmc.modcraftmod.mixin;
 
 import fr.modcraftmc.modcraftmod.ModcraftModReborn;
+import fr.modcraftmc.modcraftmod.client.screen.JoiningWorldBridgeScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.ReceivingLevelScreen;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.Connection;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
@@ -16,11 +23,15 @@ import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 @Mixin(Minecraft.class)
-public class MinecraftMixin {
+public abstract class MinecraftMixin {
 
     @Shadow @Nullable public LocalPlayer player;
 
     @Shadow @Nullable public Connection pendingConnection;
+
+    @Shadow
+    @Nullable
+    public abstract ClientPacketListener getConnection();
 
     /**
      * @author manugame_
@@ -30,5 +41,20 @@ public class MinecraftMixin {
     private String createTitle() {
         Attributes manifest = ModcraftModReborn.getManifest().getMainAttributes();
         return String.format("ModcraftMC: Reborn (ver: %s-%s)", manifest.getValue("Release-Type"),  manifest.getValue("Build-Time"));
+    }
+
+    @ModifyVariable(at = @At("HEAD"), method = "setScreen", ordinal = 0, argsOnly = true)
+    public Screen setScreen(Screen screen) {
+        if (screen instanceof ReceivingLevelScreen) {
+            return null;
+        }
+        return screen;
+    }
+
+    @ModifyArg(method = "setLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;updateScreenAndTick(Lnet/minecraft/client/gui/screens/Screen;)V", opcode = Opcodes.INVOKEVIRTUAL), index = 0)
+    private Screen setLevelUpdateScreenAndTick(Screen screen) {
+        // Make sure we clean up what needs cleaning up, just that we don't set a new screen on server switches within a proxy
+        // Can't just set it to null during reconfiguration, so set an empty screen
+        return new JoiningWorldBridgeScreen();
     }
 }
