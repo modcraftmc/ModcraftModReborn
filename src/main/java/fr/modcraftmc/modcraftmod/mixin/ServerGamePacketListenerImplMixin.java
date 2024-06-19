8b98cd193972ccd0ce0d5454dcf9c1dc40a9f43f
manugame <manugameur.pro@gmail.com>
2024-06-19 16:18:59 +0200

    do not clear chat & do not send login/logout messages

---
+++ src/main/java/fr/modcraftmc/modcraftmod/mixin/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,17 @@
+package fr.modcraftmc.modcraftmod.mixin;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class ServerGamePacketListenerImplMixin {
+
+    @Redirect(method = "onDisconnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Z)V"))
+    public void disableLoginMessage(PlayerList instance, Component p_240618_, boolean p_240644_) {
+        //no op
+    }
+}
