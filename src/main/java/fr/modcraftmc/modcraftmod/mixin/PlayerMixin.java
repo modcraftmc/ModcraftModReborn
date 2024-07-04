449c3e39f09a529cfcbdcacb643b8f9e91950a74
manugame <manugameur.pro@gmail.com>
2024-07-04 18:56:50 +0200

    Fix botania items in claimed chunks

--- src/main/java/fr/modcraftmc/modcraftmod/mixin/PlayerMixin.java
+++ src/main/java/fr/modcraftmc/modcraftmod/mixin/PlayerMixin.java
@@ -1,10 +1,12 @@
 package fr.modcraftmc.modcraftmod.mixin;
 
+import dev.ftb.mods.ftbchunks.data.FTBChunksAPI;
+import dev.ftb.mods.ftbchunks.data.Protection;
+import fr.modcraftmc.modcraftmod.ModcraftModReborn;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Abilities;
@@ -12,11 +14,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.pattern.BlockInWorld;
-import net.minecraftforge.common.ForgeHooks;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
@@ -31,19 +34,10 @@ public abstract class PlayerMixin extends LivingEntity {
      * @author
      * @reason
      */
-    @Overwrite
-    public boolean mayUseItemAt(BlockPos p_36205_, Direction p_36206_, ItemStack p_36207_) {
-        if (this.abilities.mayBuild) {
-            return true;
-        } else {
-            BlockPos blockpos = p_36205_.relative(p_36206_.getOpposite());
-            BlockInWorld blockinworld = new BlockInWorld(this.level, blockpos, false);
-            InteractionResult result = ForgeHooks.onItemRightClick((Player) (Object) this, InteractionHand.MAIN_HAND);
-            if (result == InteractionResult.FAIL) {
-                return false;
-            }
-            return p_36207_.hasAdventureModePlaceTagForBlock(this.level.registryAccess().registryOrThrow(Registry.BLOCK_REGISTRY), blockinworld);
-        }
+    @Inject(method = "mayUseItemAt", at = @At("HEAD"), cancellable = true)
+    public void mayUseItemAt(BlockPos p_36205_, Direction p_36206_, ItemStack p_36207_, CallbackInfoReturnable<Boolean> cir) {
+        boolean shouldPrevent =FTBChunksAPI.getManager().protect((Player) (Object) this, InteractionHand.MAIN_HAND, p_36205_, Protection.INTERACT_BLOCK, null);
+        if (shouldPrevent)
+            cir.setReturnValue(false);
     }
-
 }
